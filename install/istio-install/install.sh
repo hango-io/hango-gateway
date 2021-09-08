a2fbe4dd51774de01bd7ff00ac2979a2b6914310
YonkaFang <yonka.fang@foxmail.com>
2021-09-08 17:14:34 +0800

    Update istio-install/install.sh to match corrent slime version

--- install/istio-install/install.sh
+++ install/istio-install/install.sh
@@ -3,5 +3,5 @@
 kubectl create ns hango-system
 kubectl create ns mesh-operator
 
-kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/init/crds.yaml
-kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/master/install/init/slime-boot-install.yaml
+kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/v0.1/install/init/crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/slime-io/slime/v0.1/install/init/slime-boot-install.yaml
