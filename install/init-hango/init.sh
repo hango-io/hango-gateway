1bb02edc93295deab65c4fdfdc114e3a4b2eb191
hanjiahao <xidianhanjiahao@gmail.com>
2022-08-04 17:08:47 +0800

    update hango test and install

--- install/init-hango/init.sh
+++ install/init-hango/init.sh
@@ -1,19 +1,36 @@
-HANGO_NAMESPACE=hango-system
-HANGO_PORTAL=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-portal | awk '{print $3}'`
-HANGO_API_PLANE=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-api-plane | awk '{print $3}'`
-HANGO_PROXY=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-proxy | awk '{print $3}'`
-result=$(curl -s -w "%{http_code}" -o /dev/null -X POST "http://${HANGO_PORTAL}:80/gdashboard?Action=CreateGateway&Version=2018-08-09" \
--H 'Content-Type: application/json'  \
--H "x-auth-accountId:admin" \
--H "x-auth-tenantId: 1" \
--H "x-auth-projectId: 1" \
--d '{
-    "GwName":"hango_envoy_gateway",
-    "ApiPlaneAddr":"http://'"$HANGO_API_PLANE"':10880",
-    "GwClusterName":"demo-istio",
-    "GwAddr":"http://'"$HANGO_PROXY"'",
-    "GwType":"envoy",
-    "HostList": [
-        "istio.com"
-    ]
-}')
+#!/bin/bash
+
+function init_hango(){
+  HANGO_NAMESPACE=hango-system
+  HANGO_PORTAL=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-portal | awk '{print $3}'`
+  HANGO_API_PLANE=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-api-plane | awk '{print $3}'`
+  HANGO_PROXY=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-proxy | awk '{print $3}'`
+  result=$(curl -s -w "%{http_code}" -o /dev/null -X POST "http://${HANGO_PORTAL}:80/gdashboard?Action=CreateGateway&Version=2018-08-09" \
+  -H 'Content-Type: application/json'  \
+  -H "x-auth-accountId:admin" \
+  -H "x-auth-tenantId: 1" \
+  -H "x-auth-projectId: 1" \
+  -d '{
+        "GwName":"hango_envoy_gateway",
+        "ApiPlaneAddr":"http://'"$HANGO_API_PLANE"':10880",
+        "GwClusterName":"demo-istio",
+        "GwAddr":"http://'"$HANGO_PROXY"'",
+        "GwType":"envoy",
+        "VirtualHostList": [
+            {
+              "ProjectId":1,
+              "HostList": ["istio.com"],
+              "BindType":"host"
+            }
+        ]
+    }')
+  if [[ $result -eq 200 ]]; then
+      echo "Init hango success."
+  else
+      echo "Init error, init_hango can be run independently "
+      exit 1
+  fi
+}
+
+# start init hango
+init_hango
--- test/README.md
+++ test/README.md
@@ -1,2 +1,6 @@
-[//]: # (TODO)
 # Test
+## Using Python to Test Hango
+
+* Install python 2.7
+* run python hango-test.py
+
