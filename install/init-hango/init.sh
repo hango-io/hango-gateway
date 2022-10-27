2d4aade22cdb85d521d3c5f1b0a38eb1565d4302
yutao04 <yutao04@corp.netease.com>
2022-10-27 16:57:44 +0800

    feat: bundle slime install
    Signed-off-by: yutao04 <yutao04@corp.netease.com>

--- install/init-hango/init.sh
+++ install/init-hango/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+HANGO_NAMESPACE=hango-system
+
 function init_hango(){
-  HANGO_NAMESPACE=hango-system
   HANGO_PORTAL=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-portal | awk '{print $3}'`
   HANGO_API_PLANE=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-api-plane | awk '{print $3}'`
   HANGO_PROXY=`kubectl get svc -n $HANGO_NAMESPACE  | grep hango-proxy | awk '{print $3}'`
@@ -27,7 +28,11 @@ function init_hango(){
   if [[ $result -eq 200 ]]; then
       echo "Init hango success."
   else
-      echo "Init error, init_hango can be run independently "
+      echo -e "\033[31mInit error\033[0m, init_hango can be run independently \n"
+      echo -e "============================== Common mistakes ==============================\n"
+      echo -e "1.The same gateway has been created, please go to the hango-console to check"
+      echo -e "2.hango-portal is unhealthy, please check the hango-portal pod status"
+      echo -e "3.the cluster network is abnormal, please check the cluster network status\n"
       exit 1
   fi
 }
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -13,13 +13,23 @@ HANGO_SYSTEM_NAMESPACE="hango-system"
 
 
 log "start to uninstall hango components."
-helm delete hango-gateway -n hango-system
-
+helm status hango-gateway -n hango-system >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "helm component hango-gateway not exists, no need to delete"
+else
+    helm delete hango-gateway -n hango-system
+fi
 
 log "start to uninstall namespace[${HANGO_SYSTEM_NAMESPACE}]"
-kubectl get ns "${HANGO_SYSTEM_NAMESPACE}">/dev/null 2>&1
+kubectl get ns "${HANGO_SYSTEM_NAMESPACE}" >/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     kubectl delete ns "${HANGO_SYSTEM_NAMESPACE}"
+fi
+
+log "start to delete k8s crd of hango"
+kubectl get crd | grep -E "slime|istio" >/dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    kubectl get crd | grep -E "slime|istio" | awk '{print $1}' | xargs kubectl delete crd
 fi
 
 log "uninstall finished!"
